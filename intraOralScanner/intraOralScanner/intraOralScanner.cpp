9f946d668a86e98bcffa4cc584aff66b9bdba7fb
Kevin Bell <bellKevin@pm.me>
2022-10-17 20:39:02 -0600

    Add files via upload

---
+++ intraOralScanner/intraOralScanner/intraOralScanner.cpp
@@ -0,0 +1,18 @@
+// intraOralScanner.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+ // intra oral dental mouth scanner
+ // 3D scanner
+ 
+
+	// scan the mouth
+	
+	// scan the teeth
+	
+	// scan the gums
+
+}
